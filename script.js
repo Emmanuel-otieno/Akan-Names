1b4ed5a43f2ff9c1c7174ad0343890a95f8e4393
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2020-10-03 12:49:57 +0300

    update my index.css to styles.css

--- script.js
+++ script.js
@@ -3,8 +3,4 @@ var maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 function Akan(){
 
-
-
-
-
 }
