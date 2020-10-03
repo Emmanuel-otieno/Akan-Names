91ea775dd108d2b855d14ec55bd472f596baeb67
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2020-10-03 13:34:52 +0300

    rename my function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var daysOfTheWeek=[ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-function Akan(){
+function myFunction(){
+ 
 
 }
