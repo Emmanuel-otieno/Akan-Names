3a53965184dfd4ebd30f180b50613c051cfece5a
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2020-10-05 12:12:20 +0300

    add to get element by id and styled my button

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ var maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 function akanName(){
     
-    
+   var gender = document.getElementById('genders').value;
+   var date = document.getElementById('date').value;
+    date = new Date(date);
 
-
-    if(gender === 'male'){
+ if(gender === 'male'){
         alert(" Since you were born on " + daysOfTheWeek[date.getDay()] + ',' + " your name is " + maleNames[date.getDay()]);
     }
    else if(gender === 'female'){
