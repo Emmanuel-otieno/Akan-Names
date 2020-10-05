2386dbf1977527c0bfdd2ef828d30e358a65b68b
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2020-10-05 10:47:50 +0300

    add my ligical operators

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 var daysOfTheWeek=[ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-function myFunction(){
- 
+function akanName(){
+    
+    
+
+
+    if(gender === 'male'){
+        alert(" Since you were born on " + daysOfTheWeek[date.getDay()] + ',' + " your name is " + maleNames[date.getDay()]);
+    }
+   else if(gender === 'female'){
+        alert(" Since you were born on " + daysOfTheWeek[date.getDay()] + ',' + " your name is " + femaleNames[date.getDay()]);
+    }
+    else{
+        alert("Fill in the date and gender");
+    }
 
 }
